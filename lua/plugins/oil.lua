f0a424dd30ad46e5ccdd3e9593a0037832e1c999
jasper-at-windswept <jasper@windswept.digital>
2024-07-17 21:42:38 +1000

    add oil.nvim back for file management

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,17 +1,27 @@
 return {
   {
     "stevearc/oil.nvim",
-    enabled = false,
     config = function()
       require("oil").setup({
+        default_file_explorer = true,
         delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
         float = {
           width = 0.8,
           height = 0.8,
           border = "rounded",
           padding = 5,
         },
+        keymaps = {
+          ["q"] = "actions.close",
+        },
+        view_options = {
+          show_hidden = true,
+        },
       })
     end,
+    keys = {
+      { "<leader>fo", "<cmd>Oil --float<cr>", desc = "Oil" },
+    },
   },
 }
