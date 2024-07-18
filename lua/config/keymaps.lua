1bd77929f8c571beb3fb0a7441d60f36548d5428
jasper-at-windswept <jasper@windswept.digital>
2024-07-18 11:52:29 +1000

    replace toggleterm.nvim with improved custom implementation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,37 @@
 local map = vim.keymap.set
 
+map("t", "<esc>", "<C-\\><C-n>", { noremap = true, silent = true })
+map("n", "<leader>tv", function()
+  vim.cmd("vsplit")
+  vim.cmd("vertical resize -25")
+  vim.cmd("terminal")
+  vim.cmd("startinsert")
+end, { desc = "Terminal Vertical" })
+map("n", "<leader>th", function()
+  vim.cmd("split")
+  vim.cmd("resize -10")
+  vim.cmd("terminal")
+  vim.cmd("startinsert")
+end, { desc = "Terminal Horizontal" })
+
+map("n", "<leader>hv", function()
+  local win_size = vim.api.nvim_win_get_width(0)
+  if win_size < 120 then
+    vim.cmd("tabe %")
+  else
+    vim.cmd("tabclose")
+  end
+end)
+
+map("n", "<leader>hh", function()
+  local win_size = vim.api.nvim_win_get_height(0)
+  if win_size < 35 then
+    vim.cmd("tabe %")
+  else
+    vim.cmd("tabclose")
+  end
+end)
+
 -- better up/down
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
@@ -7,16 +39,16 @@ map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true
 map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 -- Move to window using the <ctrl> hjkl keys
--- map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
--- map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
--- map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
--- map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
 -- Resize window using <ctrl> arrow keys
--- map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
--- map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
--- map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
--- map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 map("n", "<leader>u=", "<C-w>=", { desc = "Reset Split Size", remap = true })
 
 -- Move Lines
@@ -41,10 +73,10 @@ map("n", "<leader>rt", "v^yi<lt><End>><Ignore>", { desc = "Create React Tag", re
 -- Clear search, diff update and redraw
 -- taken from runtime/lua/_editor.lua
 map(
-	"n",
-	"<leader>ur",
-	"<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
-	{ desc = "Redraw / Clear hlsearch / Diff Update" }
+  "n",
+  "<leader>ur",
+  "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+  { desc = "Redraw / Clear hlsearch / Diff Update" }
 )
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
@@ -66,11 +98,11 @@ map("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
 
 -- diagnostic
 local diagnostic_goto = function(next, severity)
-	local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-	severity = severity and vim.diagnostic.severity[severity] or nil
-	return function()
-		go({ severity = severity })
-	end
+  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    go({ severity = severity })
+  end
 end
 map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
 map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
