e6f759516cfac3156468318ca3e7cafc603c7ef6
jasper-at-windswept <jasper@windswept.digital>
2024-07-19 22:00:18 +1000

    supermaven is a pain, switching back to copilot

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,35 +2,35 @@ local map = vim.keymap.set
 
 map("t", "<esc>", "<C-\\><C-n>", { noremap = true, silent = true })
 map("n", "<leader>tv", function()
-  vim.cmd("vsplit")
-  vim.cmd("vertical resize -25")
-  vim.cmd("terminal")
-  vim.cmd("startinsert")
+	vim.cmd("vsplit")
+	vim.cmd("vertical resize -25")
+	vim.cmd("terminal")
+	vim.cmd("startinsert")
 end, { desc = "Terminal Vertical" })
 map("n", "<leader>th", function()
-  vim.cmd("split")
-  vim.cmd("resize -10")
-  vim.cmd("terminal")
-  vim.cmd("startinsert")
+	vim.cmd("split")
+	vim.cmd("resize -10")
+	vim.cmd("terminal")
+	vim.cmd("startinsert")
 end, { desc = "Terminal Horizontal" })
 
 map("n", "<leader>hv", function()
-  local win_size = vim.api.nvim_win_get_width(0)
-  if win_size < 120 then
-    vim.cmd("tabe %")
-  else
-    vim.cmd("tabclose")
-  end
-end)
+	local win_size = vim.api.nvim_win_get_width(0)
+	if win_size < 120 then
+		vim.cmd("tabe %")
+	else
+		vim.cmd("tabclose")
+	end
+end, { desc = "Toggle Vertical Terminal" })
 
 map("n", "<leader>hh", function()
-  local win_size = vim.api.nvim_win_get_height(0)
-  if win_size < 35 then
-    vim.cmd("tabe %")
-  else
-    vim.cmd("tabclose")
-  end
-end)
+	local win_size = vim.api.nvim_win_get_height(0)
+	if win_size < 35 then
+		vim.cmd("tabe %")
+	else
+		vim.cmd("tabclose")
+	end
+end, { desc = "Toggle Horizontal Terminal" })
 
 -- better up/down
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
@@ -73,10 +73,10 @@ map("n", "<leader>rt", "v^yi<lt><End>><Ignore>", { desc = "Create React Tag", re
 -- Clear search, diff update and redraw
 -- taken from runtime/lua/_editor.lua
 map(
-  "n",
-  "<leader>ur",
-  "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
-  { desc = "Redraw / Clear hlsearch / Diff Update" }
+	"n",
+	"<leader>ur",
+	"<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
+	{ desc = "Redraw / Clear hlsearch / Diff Update" }
 )
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
@@ -98,11 +98,11 @@ map("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
 
 -- diagnostic
 local diagnostic_goto = function(next, severity)
-  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-  severity = severity and vim.diagnostic.severity[severity] or nil
-  return function()
-    go({ severity = severity })
-  end
+	local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+	severity = severity and vim.diagnostic.severity[severity] or nil
+	return function()
+		go({ severity = severity })
+	end
 end
 map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
 map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -5,6 +5,7 @@ return {
 	},
 	{
 		"supermaven-inc/supermaven-nvim",
+		commit = "df3ecf7",
 		config = function()
 			require("supermaven-nvim").setup({})
 		end,
