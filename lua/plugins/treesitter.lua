9da96608c18a5d2d62eef34e844d2e14beddcb9d
jasper-at-windswept <jasper@windswept.digital>
2024-11-03 07:41:05 +1100

    slight tweaks

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,52 +1,53 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		dependencies = {
-			"EmranMR/tree-sitter-blade",
-		},
-		build = ":TSUpdate",
-		config = function()
-			-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-			-- parser_config.blade = {
-			--   install_info = {
-			--     url = "https://github.com/EmranMR/tree-sitter-blade",
-			--     files = { "src/parser.c" },
-			--     branch = "main",
-			--   },
-			--   filetype = "blade",
-			-- }
-			-- vim.filetype.add({
-			--   pattern = {
-			--     [".*%.blade%.php"] = "blade",
-			--   },
-			-- })
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"typescript",
-					"javascript",
-					"tsx",
-					"svelte",
-					"html",
-					"css",
-					"vim",
-					"vimdoc",
-					"lua",
-					"luadoc",
-					"markdown",
-					"bash",
-					"jsonc",
-					"json",
-					"xml",
-					"yaml",
-					"diff",
-					"go",
-					"gomod",
-					"gowork",
-					"gosum",
-				},
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "EmranMR/tree-sitter-blade",
+    },
+    build = ":TSUpdate",
+    config = function()
+      -- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      -- parser_config.blade = {
+      --   install_info = {
+      --     url = "https://github.com/EmranMR/tree-sitter-blade",
+      --     files = { "src/parser.c" },
+      --     branch = "main",
+      --   },
+      --   filetype = "blade",
+      -- }
+      -- vim.filetype.add({
+      --   pattern = {
+      --     [".*%.blade%.php"] = "blade",
+      --   },
+      -- })
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "typescript",
+          "javascript",
+          "tsx",
+          "svelte",
+          "html",
+          "css",
+          "vim",
+          "vimdoc",
+          "lua",
+          "luadoc",
+          "markdown",
+          "bash",
+          "jsonc",
+          "json",
+          "xml",
+          "yaml",
+          "diff",
+          "go",
+          "gomod",
+          "gowork",
+          "gosum",
+          "yuck",
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
 }
